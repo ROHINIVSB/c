0713522f18d0e5f163912a87545e3678779b9f82
ROHINIVSB <30316861+ROHINIVSB@users.noreply.github.com>
2017-07-20 17:30:44 +0530

    ex1.c
    zero/=ve/-ve

---
+++ ex1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+if(n==0)
+printf("\n The given number is zero");
+else if(n>0)
+printf("\n The given number is positive");
+else
+printf("\n The given number is negative");
+return 0;
+}
